42abf52a702a0785071d5d57f44d39e0b0b42d9e
Piansaen <pian2024.dev@gmail.com>
2025-02-09 13:06:45 +0700

    learn1

--- pylearn1.py
+++ pylearn1.py
@@ -1,3 +1,4 @@
+"""
 x =  5
 y = "John"
 print(x)
@@ -15,4 +16,24 @@ a = 4
 A = "Sally"
 print(a)
 print(A)
+
+x,y,z = "Orange", "Banana", "Cherry"    # Assign values to multiple variables in one line
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"    # Assign the same value to multiple variables in one line   
+print(x)
+print(y)
+print(z)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+"""
+x = 4
+y = 5
+print(x + y)
+print(x,y)
 
